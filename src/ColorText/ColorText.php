f9024a2c527544fe45c09891f0c16146bd129aa0
Heromine <jonnimperato@yahoo.it>
2014-10-16 14:06:35 +0200

    Update ColorText.php

--- src/ColorText/ColorText.php
+++ src/ColorText/ColorText.php
@@ -15,18 +15,26 @@ use pocketmine\event\player\PlayerJoinEvent;
 class ColorText extends PluginBase implements Listener{
    private static $coloredChatPlayers=[]; //<- variable should be static for use self.
    private static $config;
-   
-   public function onLoad() {
-    $this->getLogger()->info(TextFormat::AQUA . "Loading ColorText v1.2.0...");
-  }
 
+public function onLoad() {
+-    $this->getLogger()->info(TextFormat::AQUA . "Loading ColorText v1.2.0...");
+-  }
    public function onEnable(){
 	     @mkdir($this->getDataFolder()); //<-Create plugin directory
 	     $this->saveDefaultConfig(); //<-Save default config
 	     self::$config = $this->getConfig()->getAll(); //<- Get config
+	     //Checking config variables
+	     if(!isset(self::$config["enabled"]) || !isset(self::$config["name"]) || !isset(self::$config["message"])){
+		       //Saving Defaults
+		       self::$config["enabled"] = true;
+		       self::$config["name"] = "§2";
+		       self::$config["message"] = "§d";
+		       $this->getConfig()->setAll(self::$config);
+		       $this->getConfig()->save();
+		    }
 	     if(self::$config["enabled"] != true){
 		       $this->getLogger()->info("Plugin disabled");
-		       $this->getServer()->getPluginManager()->disablePlugin($this);
+		       $this->getPluginLoader()->disablePlugin($this);
 		    }else{
          $this->getServer()->getPluginManager()->registerEvents($this, $this);
       }
@@ -52,7 +60,7 @@ class ColorText extends PluginBase implements Listener{
       $message = $event->getMessage();
       foreach($this->getServer()->getOnlinePlayers() as $players){
          if(isset(self::$coloredChatPlayers[$players->getName()])){ //<-self:: used to get/set a shared variable
-            $players->sendMessage(self::$config["name"]."<".$player->getName().">ยงd ".self::$config["message"].$message);//setMessage doesnt work in BigBrother?
+            $players->sendMessage(self::$config["name"]."<".$player->getName().">§d ".self::$config["message"].$message);//setMessage doesnt work in BigBrother?
          }else{
             $players->sendMessage("<".$player->getName()."> ".$message);
          }
